dd7d56baee292f08961c7bc9fbc52d1924570247
Jacques <tangotel.tellier@gmail.com>
2022-10-26 16:02:12 +0200

    vue js

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+import BaseButton from "./components/BaseButton";
+import AsyncButton from "./components/AsyncButton"
+
+export default {
+  name: 'App',
+  components: { AsyncButton, BaseButton },
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
