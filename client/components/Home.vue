519ea1efd78fa9bb1e6583fa1ebac9c49cebf6ce
Jacques <tangotel.tellier@gmail.com>
2022-11-16 14:35:47 +0100

    début de sélection d'alerte historisé

--- client/components/Home.vue
+++ client/components/Home.vue
@@ -3,18 +3,21 @@
   <div>
     <div class="container-fluid h-100">
       <div class="row justify-content-center h-100">
-        <div class="col-lg-3 hidden-md-down" id="history">
+        <div class="col-xl-3 hidden-md-down" id="history">
           <div class="row">
             <h1>Page d'accueil</h1>
           </div>
           <div class="row">
             <section v-for="alert in alerts" :key="alert.id">
-              <p>Alert détecté le {{ alert.time }} - zone {{ alert.zone }}</p>
+              <div class="alerts" v-onclick="displayAlert(alert)">
+                <p>Alert détecté le {{ alert.time }} - zone {{ alert.zone }}</p>
+              </div>
             </section>
           </div>
         </div>
-        <div class="col-lg-9 no-float" id="alert">
+        <div class="col-xl-9 no-float" id="alert">
           <h1 v-if="newalert.id">ALERT ZONE {{ newalert.zone }}</h1>
+          <div v-if="alert">{{ alert.zone }}</div>
         </div>
       </div>
     </div>
@@ -30,11 +33,13 @@ module.exports = {
     },
     data () {
       return {
-        
+        alert : {}
       }
     },
     methods: {
-      
+      displayAlert (alert) {
+        this.alert = alert
+      }
     }
   }
 
@@ -49,6 +54,12 @@ module.exports = {
   #alert {
     background-color: grey;
     height: 100%;
+  }
+
+  .alerts {
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid black;
   }
 
 </style>
